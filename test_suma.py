f8b942f6d90f46e572b7759744364c66248e1c58
abramorales <abram728@gmail.com>
2024-10-03 17:53:50 -0600

    Update sentencias de prueba para ver si el codigo es correcto

--- test_suma.py
+++ test_suma.py
@@ -1,7 +1,7 @@
 from suma import sum
 
 def test_suma():
-    assert sum(3,5) == 8
-    assert sum(0,1) == 1
-    assert sum(-1,-5) == -6
+    assert sum(10,5) == 15
+    assert sum(1,1) == 2
+    assert sum(1,-5) == -4
     assert sum(0,0) == 0
