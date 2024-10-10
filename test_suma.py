cbf90abdfdeaf73d1921b6e59e7a20e8b74976bb
abramorales <abram728@gmail.com>
2024-10-09 18:49:55 -0600

    3er ejemplo de Bizneo Linter ajuste a archivos pyhton con errores

--- test_suma.py
+++ test_suma.py
@@ -1,7 +1,9 @@
 from suma import sum
 
+
 def test_suma():
-    assert sum(10,5) == 15
-    assert sum(1,1) == 2
-    assert sum(1,-5) == -4
-    assert sum(0,0) == 0
+    assert sum(10, 5) == 15
+    assert sum(1, 1) == 2
+    assert sum(1, -5) == -4
+    assert sum(0, 0) == 0
+    
